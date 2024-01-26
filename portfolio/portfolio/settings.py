8a5ddb85f7a4020f36595c14d0b9817932f6cf55
Giru-03 <girendra654singh@gmail.com>
2024-01-26 15:21:23 +0530

    vercel settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@mz61^j#3gnrh!a-hb$0+scm6arc_q@(8lt^e98e1*y4uuiy9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+import os
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
