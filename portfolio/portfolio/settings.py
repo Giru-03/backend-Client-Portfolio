683f64c4e81e6b593a862e70cc813b39bf424d6e
Giru-03 <girendra654singh@gmail.com>
2024-01-30 20:30:36 +0530

    new

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
